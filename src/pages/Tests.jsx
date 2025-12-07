d9d6af9e584dc39ede982848d0124765d20d03e1
Anuj Lowanshi <anujlowanshi3@gmail.com>
2025-12-07 22:46:02 +0530

    Day end commit

--- src/pages/Tests.jsx
+++ src/pages/Tests.jsx
@@ -1,23 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import api from "../api/axios";
 
 export default function Tests() {
-  const [tests] = useState(["asdf", "asdes"]);
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    const fetchTests = async () => {
+      try {
+        const res = await api.get("test/all_tests");
+        setTests(res.data);
+      } catch (err) {
+        console.error("Error fetching tests:", err);
+      }
+    };
+
+    fetchTests();
+  }, []);
 
   return (
     <div className="max-w-2xl mx-auto mt-10 px-4">
-
-      <h1 className="text-4xl font-bold mb-3 text-gray-900">
-        Available Tests
-      </h1>
-
+      <h1 className="text-4xl font-bold mb-3 text-gray-900">Available Tests</h1>
       <p className="text-gray-600 mb-8">
         Start your quiz journey below. Choose a test and challenge yourself!
       </p>
 
       <div className="space-y-4">
         {tests.map((e, i) => (
-          <div onClick={()=>window.location.href = `/test/${e}`}
+          <div
             key={i}
+            onClick={() => (window.location.href = `/test/${e}`)}
             className="p-5 bg-white shadow-md rounded-xl border border-gray-100 hover:shadow-lg hover:-translate-y-1 transition cursor-pointer"
           >
             <h2 className="text-xl font-semibold text-gray-800">{e}</h2>
@@ -25,7 +36,6 @@ export default function Tests() {
           </div>
         ))}
       </div>
-
     </div>
   );
 }
